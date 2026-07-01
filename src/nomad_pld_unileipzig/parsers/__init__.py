d2e0bdf9526bef8347b5f9c554f3967eb57f52eb
JonathanNoky <jonathan.noky@cpfs.mpg.de>
2026-07-01 10:21:55 +0200

    modifying entrypoint of parser

--- src/nomad_pld_unileipzig/parsers/__init__.py
+++ src/nomad_pld_unileipzig/parsers/__init__.py
@@ -4,8 +4,7 @@ from nomad_json_parser.parsers import ROCrateParserEntryPoint
 ro_crate_parser_pld = ROCrateParserEntryPoint(
     name='JsonParser for ROCrate files.',
     description="""Parser for ROCrate files.""",
-    level=1,
-    mainfile_name_re=r'.*ro-crate-metadata.json$',
-    mainfile_contents_re=r'.*#category-PLD Process.*',
+    mainfile_name_re=r'.*ro-crate-metadata.json$',##default
+    json_matching_re=r'.*#category-PLD Process.*',
     json_matching_key="pld_leipzig_jsonld",
 )
